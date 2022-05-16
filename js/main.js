1e0d446735134b32460b57776f0a2d5d9904fb3d
steve <stevegomitas3@gmail.com>
2022-05-16 14:59:56 -0600

    Se mejoro el login con javascript

--- js/main.js
+++ js/main.js
@@ -1,15 +1,96 @@
 function login(){
     var loginVar = document.getElementById("loginDatoInput").value;
-    
+    var contraVar = document.getElementById("password").value;
+
+    var contraTrue = document.getElementById("contraseñaCorrecta");
+    var contraFalse = document.getElementById("contraseñaIncorrecta");
+    var noContra = document.getElementById("Nocontraseña");
+
+    var contraTrueInput = document.getElementById("contraseñaInputCorrecta");
+    var contraFalseInput = document.getElementById("contraseñaInputIncorrecta");
+    var noContraInput = document.getElementById("NocontraseñaInput");
+
+    var userTrueFalse;
+    var contraTrueFalse;
     if(loginVar == "anna0"){
-        alert('Tu nombre de usuario es correcto');
-        document.getElementById("loginDatoInput").value="";
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
+        if(contraVar == "white00"){
+            contraTrueInput.classList.add('contraseñaInputCorrecta2');
+            contraFalseInput.classList.remove('contraseñaInputIncorrecta2');
+            noContraInput.classList.remove('NocontraseñaInput2');
+            contraTrueFalse = true;
+        }else if(contraVar == ""){
+            contraFalseInput.classList.remove('contraseñaInputIncorrecta2');
+            contraTrueInput.classList.remove('contraseñaInputCorrecta2');
+            noContraInput.classList.add('NocontraseñaInput2');
+            contraTrueFalse = false;
+        }else{
+            contraFalseInput.classList.add('contraseñaInputIncorrecta2');
+            contraTrueInput.classList.remove('contraseñaInputCorrecta2');
+            noContraInput.classList.remove('NocontraseñaInput2');
+            contraTrueFalse = false;
+        }
+    }else if(loginVar == "carol1"){
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
+        if(contraVar == "orange01"){
+            contraTrueInput.classList.add('contraseñaInputCorrecta2');
+            contraFalseInput.classList.remove('contraseñaInputIncorrecta2');
+            noContraInput.classList.remove('NocontraseñaInput2');
+            contraTrueFalse = true;
+        }else if(contraVar == ""){
+            contraFalseInput.classList.remove('contraseñaInputIncorrecta2');
+            contraTrueInput.classList.remove('contraseñaInputCorrecta2');
+            noContraInput.classList.add('NocontraseñaInput2');
+            contraTrueFalse = false;
+        }else{
+            contraFalseInput.classList.add('contraseñaInputIncorrecta2');
+            contraTrueInput.classList.remove('contraseñaInputCorrecta2');
+            noContraInput.classList.remove('NocontraseñaInput2');
+            contraTrueFalse = false;
+        }
+    }else if(loginVar == "julia2"){
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
+    }else if(loginVar == "irene3"){
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
+    }else if(loginVar == "rachel4"){
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
+    }else if(loginVar == "sophie5"){
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
+    }else if(loginVar == "wendy6"){
+        contraTrue.classList.add('contraseñaCorrecta2');
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = true;
     }else if(loginVar == ""){
-        alert('Coloca tu nombre antes de continuar');
-        document.getElementById("loginDatoInput").focus;
+        contraFalse.classList.remove('contraseñaIncorrecta2');
+        contraTrue.classList.remove('contraseñaCorrecta2');
+        userTrueFalse = false;
+        noContra.classList.add('Nocontraseña2');
     }else{
-        alert('Tu nombre de usuario es incorrecto');
+        contraFalse.classList.add('contraseñaIncorrecta2');
+        contraTrue.classList.remove('contraseñaCorrecta2');
+        noContra.classList.remove('Nocontraseña2');
+        userTrueFalse = false;
     }
-    console.log(loginVar);
+    console.log(loginVar, userTrueFalse);
+
 
 }
